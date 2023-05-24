766c37443e1d5e092543abe5a25225ca0c88e086
seungahpyun <seungahpyun@icloud.com>
2023-05-24 12:33:58 +0200

    Course information step6

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,61 +1,42 @@
-import React from 'react';
-
-const Header = ({course}) => {
-  return (
-    <h1>{course}</h1>
-  )
-}
-
-const Content = ({parts}) => {
+const Course = ({ course }) => {
   return (
     <div>
-      <Part part= {parts[0]} />
-      <Part part= {parts[1]} />
-      <Part part= {parts[2]} />
+      <h1>{course.name}</h1>
+      <p>
+        {course.parts.map(part => (
+          <p key={part.id}>
+            {part.name} {part.exercises}
+          </p>))}
+      </p>
     </div>
   )
 }
 
-const Total = ({parts}) => {
-  return (
-  <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
-  )
-}
-
-const Part = ({part}) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  )
-}
 
 const App = () => {
   const course = {
-    name: 'Half Stack Application Development',
+    id: 1,
+    name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
-  return (
-  <div>
-    <Header course={course.name} />
-    <Content parts={course.parts}/>
-    <Total parts={course.parts}/>
-  </div>
-  )
+  return <Course course={course} />
 }
 
-export default App;
+export default App
