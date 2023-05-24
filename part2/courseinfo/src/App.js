ed5e4ad4d74502ebdd094436ee0216e1d1d65d9a
seungahpyun <seungahpyun@icloud.com>
2023-05-24 14:43:49 +0200

    course info

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,42 +1,13 @@
-const Course = ({ course }) => {
+import React from 'react'
+import Course from './components/Course'
+
+const App = ({courses}) => {
   return (
     <div>
-      <h1>{course.name}</h1>
-      <p>
-        {course.parts.map(part => (
-          <p key={part.id}>
-            {part.name} {part.exercises}
-          </p>))}
-      </p>
+      <Course courses={courses} />
     </div>
   )
-}
 
-
-const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
-
-  return <Course course={course} />
 }
 
 export default App
