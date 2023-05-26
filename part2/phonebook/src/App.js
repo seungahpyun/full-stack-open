aa21012b3924e19dedad06eb01dc7eeefe89f9df
seungahpyun <seungahpyun@icloud.com>
2023-05-26 17:47:32 +0200

    filter

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -9,37 +9,62 @@ const Button = ({Click, text}) => (
 
 const DisplayName = ({persons}) => {
   const personList = Object.entries(persons)
-  console.log(personList)
   return (
     <div>
-      {personList.map(([key , value]) =>
-        <div>
-          {key} {value}
+      {personList.map(([name , number]) =>
+        <div key={name}>
+          {name} {number}
         </div>
       )}
     </div>
   )
 }
 
+const Filtered = ({persons, filter}) => {
+  const personList = Object.keys(persons)
+  if (filter in personList) {
+    return (
+      <div>
+        {personList.map(name =>
+          <div key={name}>
+            {name} {persons[name]}
+          </div>
+        )}
+      </div>
+    )
+  }
+  // return (
+  //   <div>
+  //     {filter}
+  //   </div>
+  // )
+}
+
+
 const App = () => {
   const [persons, setPersons] = useState({})
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
 
   const addPerson = (event) => {
     event.preventDefault()
   }
 
-
   const handleNewName = (event) => {
     setNewName(event.target.value)
   }
 
-
   const handleNewNumber = (event) => {
     setNewNumber(event.target.value)
   }
+
+  const handleFilter = (event) => {
+    const search = event.target.value;
+    setFilter(search)
+  }
+
 
 
   const handleNameClick = () => {
@@ -49,16 +74,26 @@ const App = () => {
     if (updatedName in persons){
       alert(`${updatedName} is already added to the phonebook`)
     }
-    if (!(updatedName in persons)){
-      const updatedPersons = {...persons, [updatedName] : updatedNumber}
-      setPersons(updatedPersons)
+
+    if (updatedName === '' || updatedNumber === '') {
+      alert(`pleaese enter a name and number`)
+    } else {
+        const updatedPersons = {...persons, [updatedName] : updatedNumber}
+        setPersons(updatedPersons)
     }
   }
-
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          filter shown with {" "}
+          <input name="filtered" value={filter} onChange={handleFilter}/>
+        </div>
+      </form>
+      <Filtered persons={persons} filter={filter}/>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name:{" "}
