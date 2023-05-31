54e6c4bcd346cf73ec395faf2a3db51382993af9
seungahpyun <seungahpyun@icloud.com>
2023-05-31 15:08:23 +0200

    phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
