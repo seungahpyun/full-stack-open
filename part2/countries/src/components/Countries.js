9260bca16153eec87c3418b8f8bf5f06199dec04
seungahpyun <seungahpyun@icloud.com>
2023-06-01 16:08:39 +0200

    countries 2.18

---
+++ part2/countries/src/components/Countries.js
@@ -0,0 +1,12 @@
+const Countries = ({ countriesToShow, setCountriesToShow }) => {
+  if (countriesToShow.length === 1) return null;
+
+  return countriesToShow.map((country) => (
+    <div key={country.name.official}>
+      {country.name.common}{" "}
+      <button onClick={() => setCountriesToShow([country])}>show</button>
+    </div>
+  ))
+}
+
+export default Countries;
