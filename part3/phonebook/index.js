dce1b66b698e51c68e4ed2b994525f50f63c0a5b
seungahpyun <seungahpyun@icloud.com>
2023-06-05 13:04:44 +0200

    phonebook 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,8 +3,15 @@ const morgan = require('morgan')
 const app = express()
 
 
-app.use(morgan('combined'))
 app.use(express.json())
+morgan.token("data", (req) => {
+  return req.method === "POST" ? JSON.stringify(req.body) : " "}
+)
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :data")
+)
+
 
 let persons=[
   {
