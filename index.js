75fc20781ab0c77013da5247094ffad4bcf12b58
seungahpyun <seungahpyun@icloud.com>
2023-06-07 16:54:28 +0200

    commit

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
 const mongoose = require('mongoose')
-const mongoUrl = process.env.MONGODB_URI
 
-mongoose.connect(mongoUrl)
+
+mongoose.connect(config.MONGODB_URI)
   .then(() => {
     console.log('connected to MongoDB')
   })
@@ -43,7 +45,6 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
