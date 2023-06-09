bc28c7b3e95b43789de61976992c7496d479484f
seungahpyun <seungahpyun@icloud.com>
2023-06-09 13:56:57 +0200

    bloglist 4.8 ini

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+const helper = require('./blog_api_test_helper')
+
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+test('notes are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('unique identifier property of the blog posts is named id', async () => {
+  const response = await api.get('/api/blogs')
+  const ids = response.body.map(r=> r.id)
+  for (let id of ids) {
+    expect(id).toBeDefined()
+  }
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
