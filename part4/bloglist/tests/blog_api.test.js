a195704e1c7ed225e7d5877fae61a344e083af60
seungahpyun <seungahpyun@icloud.com>
2023-06-09 16:07:55 +0200

    bloglist blog_api_test prettier

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -5,8 +5,6 @@ const api = supertest(app)
 const Blog = require('../models/blog')
 const helper = require('../utils/blog_api_test_helper')
 
-
-
 beforeEach(async () => {
   await Blog.deleteMany({})
 
@@ -16,14 +14,12 @@ beforeEach(async () => {
   }
 })
 
-
 test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 })
-
 
 test('unique identifier property of the blog posts is named id', async () => {
   const response = await api.get('/api/blogs')
