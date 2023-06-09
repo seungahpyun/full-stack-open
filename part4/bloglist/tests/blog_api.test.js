552a8e0eb2d4e6ba391890a93bdb6a45b2db334a
seungahpyun <seungahpyun@icloud.com>
2023-06-09 16:00:02 +0200

    bloglist 4.9

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -17,25 +17,42 @@ beforeEach(async () => {
 })
 
 
-test('notes are returned as json', async () => {
+test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 })
 
-// test('all Blogs are returned', async () => {
-//   const response = await api.get('/api/blogs')
-//   expect(response).toHaveLength(helper.initialBlogs.length)
-// })
 
-// test('unique identifier property of the blog posts is named id', async () => {
-//   const response = await api.get('/api/blogs')
-//   const ids = response.body.map(r=> r.id)
-//   for (let id of ids) {
-//     expect(id).toBeDefined()
-//   }
-// })
+test('unique identifier property of the blog posts is named id', async () => {
+  const response = await api.get('/api/blogs')
+  const ids = response.body.map(r=> r.id)
+  for (let id of ids) {
+    expect(id).toBeDefined()
+  }
+})
+
+describe('addition of a new blog', () => {
+  test('a valid blog can be added', async () => {
+    await api
+      .post('/api/blogs')
+      .send({
+        author: 'Test Author',
+        title: 'Test Blog',
+        url: 'http://testblog.com'
+      })
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+      const finalBlogs = await helper.blogsInDb()
+      expect(finalBlogs).toHaveLength(helper.initialBlogs.length + 1)
+      const titles = finalBlogs.map(r => r.title)
+      expect(titles).toContain('Test Blog')
+  })
+})
+
+
+
 
 afterAll(async () => {
   await mongoose.connection.close()
