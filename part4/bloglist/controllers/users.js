56472b6643215d5bf05f2cf97cffea149115b367
seungahpyun <seungahpyun@icloud.com>
2023-06-12 13:39:57 +0200

    bloglist 4.16

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -10,19 +10,22 @@ usersRouter.get('/', async (request, response) => {
 
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
+  if (username === undefined || password === undefined) {
+    return response.status(400).json({ error: 'username and password required' })
+  } else if (username.length < 3 || password.length < 3) {
+    return response.status(400).json({ error: 'username and password must be at least 3 characters long' })
+  } else {
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
 
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(password, saltRounds)
-
-  const user = new User({
-    username,
-    name,
-    passwordHash,
-  })
-
-  const savedUser = await user.save()
-
-  response.status(201).json(savedUser)
+    const user = new User({
+      username,
+      name,
+      passwordHash,
+    })
+    const savedUser = await user.save()
+    response.status(201).json(savedUser)
+  }
 })
 
 module.exports = usersRouter
