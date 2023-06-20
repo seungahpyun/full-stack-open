beb9b1c0f8a151a4a472e50e2edf9a2d485eeb5c
seungahpyun <seungahpyun@icloud.com>
2023-06-20 15:17:08 +0200

    part6 6.7

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,35 +1,12 @@
-import { useSelector, useDispatch } from 'react-redux'
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
-
-  const addAnecdote = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    dispatch({ type: 'NEW_ANECDOTE', data: { content, votes: 0 } })}
-
-  const toggleImportanceOf = (id) => {
-    dispatch({ type: 'VOTE', data: { id } })
-  }
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      <form onSubmit={addAnecdote}>
-        <input name='anecdote' />
-        <button type='submit'>add</button>
-      </form>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          {anecdote.content}
-          <div>
-            has {anecdote.votes} votes
-            <button onClick={() => toggleImportanceOf(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteForm />
+      <AnecdoteList />
     </div>
   )
 }
