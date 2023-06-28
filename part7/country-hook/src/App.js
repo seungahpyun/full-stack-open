140ba43777fc6589ceb3e31d2ca4385611781c39
seungahpyun <seungahpyun@icloud.com>
2023-06-28 14:27:59 +0200

    part7 7.7

--- part7/country-hook/src/App.js
+++ part7/country-hook/src/App.js
@@ -1,50 +1,6 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-
-const useField = (type) => {
-  const [value, setValue] = useState('')
-
-  const onChange = (event) => {
-    setValue(event.target.value)
-  }
-
-  return {
-    type,
-    value,
-    onChange
-  }
-}
-
-const useCountry = (name) => {
-  const [country, setCountry] = useState(null)
-
-  useEffect(() => {})
-
-  return country
-}
-
-const Country = ({ country }) => {
-  if (!country) {
-    return null
-  }
-
-  if (!country.found) {
-    return (
-      <div>
-        not found...
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      <h3>{country.data.name} </h3>
-      <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
-    </div>
-  )
-}
+import { useCountry,useField } from "./hooks"
+import Country from "./components/Country"
+import { useState } from "react"
 
 const App = () => {
   const nameInput = useField('text')
---
+++ part7/country-hook/src/hooks/index.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+export const useCountry = (name) => {
+  const [country, setCountry] = useState(null)
+
+  useEffect(() => {
+    if (name) {
+      axios
+        .get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+        .then((response) => {
+          setCountry(
+            { data: response.data[0], found: true }
+          )
+        })
+        .catch((error) => {
+          setCountry({ found: false })
+        })
+    }
+  }, [name])
+
+  return country
+}
