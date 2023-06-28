57c6adf01e895fffe1f60f9ea11b3de10327ea16
seungahpyun <seungahpyun@icloud.com>
2023-06-28 14:59:13 +0200

    part7 7.8

---
+++ part7/ultimate-hooks/src/hooks/index.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+export const useResource = (baseUrl) => {
+  const [resources, setResources] = useState([])
+
+  useEffect(() => {
+    const fetchResources = async () => {
+      const response = await axios.get(baseUrl)
+      setResources(response.data)
+    }
+    fetchResources()
+  }, [baseUrl])
+
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    setResources([...resources, response.data])
+  }
+
+  const service = {
+    create
+  }
+
+  return [
+    resources, service
+  ]
+}
