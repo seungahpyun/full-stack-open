418a11e3c62ce129ac7b78a34d805777651b97ad
seungahpyun <seungahpyun@icloud.com>
2023-07-10 12:50:20 +0200

    part8 8.4

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -110,7 +110,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String!): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -119,7 +119,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => books.filter(book => book.author === args.author),
     allAuthors: () => authors,
   },
   Author : {
