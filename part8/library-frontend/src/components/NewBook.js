cd5ee97a4061c1b329bf8c28d2bb183c4a0d25db
seungahpyun <seungahpyun@icloud.com>
2023-07-10 15:40:20 +0200

    part8 8.10

--- part8/library-frontend/src/components/NewBook.js
+++ part8/library-frontend/src/components/NewBook.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-// import { useMutation } from '@apollo/client'
-// import { ALL_AUTHORS, ALL_BOOKS, CREATE_BOOK } from '../queries'
+import { useMutation } from '@apollo/client'
+import { ALL_AUTHORS, ALL_BOOKS, CREATE_BOOK } from '../queries'
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
@@ -8,9 +8,9 @@ const NewBook = (props) => {
   const [published, setPublished] = useState('')
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
-  // const [createBook] = useMutation(CREATE_BOOK, {
-  //   refetchQueries: [{ query: ALL_AUTHORS }, { query: ALL_BOOKS }],
-  // })
+  const [createBook] = useMutation(CREATE_BOOK, {
+    refetchQueries: [{ query: ALL_AUTHORS }, { query: ALL_BOOKS }],
+  })
 
   if (!props.show) {
     return null
@@ -19,7 +19,7 @@ const NewBook = (props) => {
   const submit = async (event) => {
     event.preventDefault()
 
-    // createBook({ variables: { title, author, published, genres } })
+    createBook({ variables: { title, author, published, genres } })
 
     console.log('add book...')
     setTitle('')
@@ -56,7 +56,7 @@ const NewBook = (props) => {
           <input
             type="number"
             value={published}
-            onChange={({ target }) => setPublished(target.value)}
+            onChange={({ target }) => setPublished(parseInt(target.value))}
           />
         </div>
         <div>
--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -21,14 +21,14 @@ export const ALL_BOOKS = gql`
   }
 `
 
-
-
-// export const CREATE_BOOK = gql`
-//   mutation createBook($title: String!, $author: String!, $published: Int!, $genres: [String]!){
-//     addBook(title: $title, author: $author, published: $published, genres: $genres){
-//       title
-//       author
-//       published
-//     }
-//   }
-// `
+export const CREATE_BOOK = gql`
+mutation AddBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
+  addBook(title: $title, author: $author, published: $published, genres: $genres) {
+    author
+    genres
+    id
+    published
+    title
+  }
+}
+`
