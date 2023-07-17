dd4fbbce30215cf08965c9e4dae601a4e9c3a3e4
seungahpyun <seungahpyun@icloud.com>
2023-07-17 14:32:46 +0200

    part9 9.5

--- part9/exercises/bmiCalculator.ts
+++ part9/exercises/bmiCalculator.ts
@@ -25,3 +25,5 @@ try {
   }
   console.log(errorMessage)
 }
+
+export { calculateBmi }
--- part9/exercises/index.ts
+++ part9/exercises/index.ts
@@ -1,11 +1,22 @@
-import express from 'express';
+import express from 'express'
 const app = express()
 
-import { Request, Response } from 'express';
+import { Request, Response } from 'express'
+import { calculateBmi } from './bmiCalculator'
 
 app.get('/hello', (_req: Request, res: Response) => {
   res.send('Hello Full Stack!')
 })
+
+app.get('/bmi', (req: Request, res: Response) => {
+  const { height, weight } = req.query
+  if (!height || !weight) {
+    res.status(400).json({ error: "malformatted parameters" })
+  }
+  const bmi = calculateBmi(Number(height), Number(weight))
+  res.json({ weight, height, bmi })
+})
+
 
 const PORT = 3003
 
