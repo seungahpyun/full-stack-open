016c3ac0feae357fb8aaf1a906edc8486db2cf72
seungahpyun <seungahpyun@icloud.com>
2023-07-17 12:00:47 +0200

    part9 9.2

---
+++ part9/exercises/exerciseCalculator.ts
@@ -0,0 +1,39 @@
+interface ExerciseValues {
+  periodLength: number,
+  trainingDays: number,
+  success: boolean,
+  rating: number,
+  ratingDescription: string,
+  target: number,
+  average: number
+}
+
+const calculateExercises = (dailyHours: Array<number>, target: number): ExerciseValues => {
+  const periodLength = dailyHours.length
+  const trainingDays = dailyHours.filter(h => h > 0).length
+  const average = dailyHours.reduce((a, b) => a + b, 0) / periodLength
+  const success = average >= target
+  const rating = success ? 3 : average >= target / 2 ? 2 : 1
+  const ratingDescription = success ? 'good job' : average >= target / 2 ? 'not too bad but could be better' : 'you need to work harder'
+
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average
+  }
+}
+
+try {
+  console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+}
+catch (error: unknown) {
+  let errorMessage = 'Something went wrong, error: '
+  if (error instanceof Error) {
+    errorMessage += error.message
+  }
+  console.log(errorMessage)
+}
