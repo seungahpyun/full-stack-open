be8e2750666e8c7653a53d7de240e330b207f9b6
seungahpyun <seungahpyun@icloud.com>
2023-07-21 13:24:31 +0200

    part9 9.14 initial commit

---
+++ part9/react-types-exercise/src/App.tsx
@@ -0,0 +1,27 @@
+const App = () => {
+  const courseName = "Half Stack application development";
+  const courseParts = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14
+    }
+  ];
+
+  return (
+    <div>
+      <Header courseName={courseName} />
+      <Content courseParts={courseParts} />
+      <Total courseParts={courseParts} />
+    </div>
+  );
+};
+
+export default App;
