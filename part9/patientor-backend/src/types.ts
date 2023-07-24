907bf0827624bf140509830dbb49e97840505bbb
seungahpyun <seungahpyun@icloud.com>
2023-07-24 13:51:45 +0200

    part9 9.22

--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -10,9 +10,6 @@ export enum Gender {
   Other = 'other'
 };
 
-export interface Entry {
-};
-
 export interface Patient {
   id: string;
   name: string;
@@ -26,3 +23,44 @@ export interface Patient {
 export type PublicPatient = Omit<Patient, 'ssn' | 'entries'>;
 
 export type NewPatient = Omit<Patient, 'id'>;
+
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose['code']>;
+};
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+};
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+};
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+};
+
+export interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+};
+
+export type EntryType = HealthCheckEntry | OccupationalHealthcareEntry | HospitalEntry;
+
+export type Entry = BaseEntry | EntryType;
